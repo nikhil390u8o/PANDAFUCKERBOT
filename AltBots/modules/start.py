7714d94177f7f0cefb836c179f39bfc242b25686
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-10-30 20:54:49 +0530

    Replace repo button with inline button and update text

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -7,7 +7,7 @@ START_BUTTON = [
         Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/ASUR_SAMRAJY_NET"),
         Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/ASUR_SAMRAJY_NET")
     ],
-    [Button.url("• ʀᴇᴘᴏ •", "https://t.me/REPO_679/2")]
+    [Button.inline("• ʀᴇᴘᴏ •", data="repo")]
 ]
 
 # List of all bot clients
@@ -17,7 +17,7 @@ async def start_handler(event):
     AltBot = await event.client.get_me()
     bot_name = AltBot.first_name
     bot_id = AltBot.id
-    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id} ),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}]\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
     TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𓆩𝐀𝐒𓆪 ꭙ 𝐉𝐄𝐑𝐑𝐘 ⌯ 𝐊𝐈𝐍𝐆💀 #𝐅𝐔𝐂𝐊𝐄𝐑](tg://openmessage?user_id=7290768963)**\n\n"
     TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
     TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
@@ -25,15 +25,31 @@ async def start_handler(event):
 
     await event.client.send_file(
         event.chat_id,
-        "https://t.me/ANIME_HUB6229/75",
+        "https://t.me/ANIME_HUB6229/80",
         caption=TEXT,
         buttons=START_BUTTON
     )
 
-# --- Callback for "Back" Button ---
+async def button_handler(event):
+    # event.data is bytes, so decode if needed
+    data = event.data.decode("utf-8")
+    
+    if data == "repo":
+        await event.answer(
+            "𝗕𝗛𝗔𝗚 𝗝𝗔𝗔 𝗟𝗢𝗗𝗘 𝗥𝗘𝗣𝗢 𝗟𝗘𝗚𝗔 𓆩𝐀𝐒𓆪 ꭙ 𝐉𝐄𝐑𝐑𝐘 ⌯ 𝐊𝐈𝐍𝐆 𝗞𝗔 𝗟𝗨𝗠𝗗 𝗟𝗘𝗟𝗘 😎\n\n ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀʟᴛʀᴏɴᴇ x ʙᴏᴛs",
+            alert=True
+        )
 
+# Attach to all bots
+for bot in BOTS:
+    bot.add_event_handler(button_handler, events.CallbackQuery)
+
+
+# --- Callback for "Back" Button ---
 
 # Attach the same /start handler to all bots
 for bot in BOTS:
     bot.add_event_handler(start_handler, events.NewMessage(pattern="/start"))
+    
+
     
